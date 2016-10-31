2d0cb23b948707e1a1eb290bc5c0972c2abacb3c
Dana <dana.lachman@gmail.com>
2016-10-31 13:52:28 -0500

    fixed errors in data file

--- src/scripts/data/CloudData.js
+++ src/scripts/data/CloudData.js
@@ -91,15 +91,15 @@ var data = {
         "stratiformis": {
           "description": "The most common type of Altocumulus, looking like flat-bottomed puffy clouds packed tightly together but separated by small rivers of sky. These can sometimes extend over the whole sky but are more common in smaller patches.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Stratocumulus_radiatus_in_Texas.jpg/1024px-Stratocumulus_radiatus_in_Texas.jpg"
-        }
+        },
         "lenticularis": {
           "description": "One of the most spectacular cloud types, altocumulus lenticularis (also known as lenticular clouds) are lens-shaped clouds that form over hilly areas. Sometimes referred to as 'spaceship clouds' they often resemble the shape of a UFO.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Flickr_-_Nicholas_T_-_Couplet.jpg/1024px-Flickr_-_Nicholas_T_-_Couplet.jpg"
-        }
+        },
         "castellanus": {
           "description": "An indicator of instability, altocumulus castellanus towers can lead to the formation of cumulonimbus thunderstorms. These are more puffy looking and are taller than they are wide, making them stand out from other altocumulus varieties.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Altocumulus-Castellanus.jpg/1024px-Altocumulus-Castellanus.jpg"
-        }
+        },
         "floccus": {
           "description": "Often spotted alongside altocumulus castellanus, altocumulus floccus are made up of slightly smaller, more ragged cloudlets. These are often found with virga hanging below.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Avenida_23_de_Maio%2C_vista_sul_-_panoramio.jpg/1024px-Avenida_23_de_Maio%2C_vista_sul_-_panoramio.jpg"
@@ -126,15 +126,15 @@ var data = {
         "humilis": {
           "description": "These are wider than they are tall, often numerous in the sky and indicate fair weather. conditions",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Colline_del_Monferrato_HDR_2012.jpg/1024px-Colline_del_Monferrato_HDR_2012.jpg"
-        }
+        },
         "mediocris": {
           "description": "These are as wide as they are tall and are usually seen amongst a variety of other cumulus variations.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/3/35/Beautiful_Day.jpg"
-        }
+        },
         "congestus": {
           "description": "These are taller than they are wide, looking like long chimneys capable of producing light showers.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Panorama_lago_di_bracciano_%283495864843%29.jpg/1024px-Panorama_lago_di_bracciano_%283495864843%29.jpg"
-        }
+        },
         "fractus": {
           "description": "These are the remnants of cumulus clouds that are breaking up or dissipating.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Flickr_-_Nicholas_T_-_Bactrian.jpg/1024px-Flickr_-_Nicholas_T_-_Bactrian.jpg"
@@ -151,7 +151,7 @@ var data = {
         "nebulosus": {
           "description": "This is a featureless, dark layer which is capable of producing drizzle.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Lake_McDonald_%285387250181%29.jpg/1024px-Lake_McDonald_%285387250181%29.jpg"
-        }
+        },
         "fractus": {
           "description": "This is a stratus layer which is starting to break up or 'dissipate', leading to breaks in the cloud.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Cloud_layer_above_Badderfjorden%2C_Kv%C3%A6nangen%2C_Troms%2C_Norway%2C_2014_August.jpg/1024px-Cloud_layer_above_Badderfjorden%2C_Kv%C3%A6nangen%2C_Troms%2C_Norway%2C_2014_August.jpg"
@@ -168,11 +168,11 @@ var data = {
         "calvus": {
           "description": "The top of the cumulonimbus is puffy, like a cumulus cloud. The water droplets at the top of the cloud tower have not frozen to become ice crystals.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/2013-09-12_11_37_02_Thunderstorms_developing_near_dessicated_ground_in_Elko%2C_Nevada.jpg/1024px-2013-09-12_11_37_02_Thunderstorms_developing_near_dessicated_ground_in_Elko%2C_Nevada.jpg"
-        }
+        },
         "capillatus": {
           "description": "The top of the cloud is fibrous but relatively contained. Water droplets have started to freeze; usually indicating rain has begun or will begin soon.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Cloud_over_Boulder_Mtn_%283684617703%29.jpg/1024px-Cloud_over_Boulder_Mtn_%283684617703%29.jpg"
-        }
+        },
         "incus": {
           "description": "The top of the cloud is fibrous and anvil shaped as the cloud has continued to grow. If the cloud reaches the top of the troposphere and still wishes to grow, it must do so outwards, creating the picturesque anvil or incus.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/d/d9/Hector_cloud_from_Gunn_Point.jpg"
@@ -189,15 +189,15 @@ var data = {
         "stratiformis": {
           "description": "The most common cloud type across the globe, these are essentially flat based layers of cloud often with a few cracks between.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Stratocumulus_stratiformis_perlucidus_translucidus.jpg/1024px-Stratocumulus_stratiformis_perlucidus_translucidus.jpg"
-        }
+        },
         "cumulogenitus": {
           "description": "These form when rising cumulus clouds encounter a temperature inversion (a warming of the air above) and spread outwards clumping together.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Flickr_-_Nicholas_T_-_Autumn_Prelude.jpg/1024px-Flickr_-_Nicholas_T_-_Autumn_Prelude.jpg"
-        }
+        },
         "castellanus": {
           "description": "These are thicker, more drizzly stratocumulus clouds. Turreted tops form when convection initiates through the stable layer allowing stratocumulus to grow upwards, potentially leading to the formation of cumulus congestus or even cumulonimbus.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Stratocumulus_castellanus_2.jpg/1024px-Stratocumulus_castellanus_2.jpg"
-        }
+        },
         "lenticularis": {
           "description": "The rarest variety of stratocumulus; lenticularis, is often spotted in hilly locations. Very different in appearance to the more spectacular altocumulus lenticularis, they form when hills produce atmospheric waves, which contribute to their lens-like shape.",
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Miscanti_Lagoon_San_Pedro_de_Atacama_Chile_Luca_Galuzzi_2006.jpg/1024px-Miscanti_Lagoon_San_Pedro_de_Atacama_Chile_Luca_Galuzzi_2006.jpg"
@@ -211,60 +211,55 @@ var data = {
       "formation": "Mammatus clouds are usually formed in association with large cumulonimbus clouds. Typically turbulence within the cumulonimbus cloud will cause mammatus to form, especially on the underside of the projecting anvil as it descends to lower levels. This reverses the usual cloud-forming process of upward growth, making for an uneven cloud base.",
       "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Austin_Mammatus_Clouds_25_May_2015.jpg/1024px-Austin_Mammatus_Clouds_25_May_2015.jpg"
     }
-  }
+  },
     "funnel": {
       "description": "A funnel cloud is a cone-shaped cloud which extends from the base of a cloud towards the ground without actually reaching the surface. In the UK they often look like thin dangling bits of rope, hanging from the cloud above. But in hotspots such as tornado alley in the USA, funnel clouds can sometimes be thicker and much more intense.",
       "formation": "A rotating column of wind draws in cloud droplets, making a region of intense low pressure visible. They are formed in the same way as a tornado building around this localised area of intensely low pressure and are typically associated with the formation of cumulonimbus thunderclouds.",
       "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/F5_tornado_funnel_cloud_Elie_Manitoba_2007.jpg/1024px-F5_tornado_funnel_cloud_Elie_Manitoba_2007.jpg"
-    }
-  }
-    "Arcus": {
+    },
+    "arcus": {
       "description": "Arcus comes from the latin word for 'arch'. There are two types of arcus clouds: shelf clouds or roll clouds. They can normally be seen beneath powerful storm clouds or cumulonimbus clouds. Shelf clouds are wedge shaped and attached to the storm cloud whereas roll clouds are a horizontal column separated from the storm cloud that can appear to be rolling. They form within warm air from near the ground which has been pushed up by the cold air exiting out of the storm cloud in the downdraft.",
       "types": {
         "shelf": {
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Cloud_over_yucatan_mexico_01.jpg/1024px-Cloud_over_yucatan_mexico_01.jpg"
-        }
+        },
         "roll": {
           "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Roll-cloud-2.JPG/1024px-Roll-cloud-2.JPG"
         }
       }
-    }
-  }
+  },
     "noctilucent": {
       "description": "Noctilucent clouds are the highest clouds in the Earth's atmosphere forming above 200,000 ft. They are typically too faint to be seen, but occasionally become visible during twilight when the sun is below the horizon and the clouds becomes illuminated. They usually take on a blue light, though will sometimes take on a red or even green appearance. It is this illumination that is alluded in the clouds name with nocti the Latin for night and lucent meaning shining.",
       "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/20150710-leuchtende-Nachtwolken-03.jpg/1024px-20150710-leuchtende-Nachtwolken-03.jpg"
-    }
-  }
+  },
     "banner": {
       "description": "A banner cloud is one of a number of different cloud types that are grouped as orographic clouds. This defines them as being caused by the shaped of the land, with orographic (from the Greek óρος meaning 'hill') referring to the relief of land and mountains.",
       "formation": "When the wind blows against a hill or mountain it is forced to lift. Air cools as it rises and the water vapour within it condenses to form a cloud. As the name implies, these clouds form in a layer and then remain also stationary as the wind continuously flows from one direction. The prevailing wind determines the particular microclimates found around a hill or mountain. Areas to the leeward side of the hill can often experience shelter, with more sunshine, less rainfall and higher temperatures than areas to the windward side. Banner clouds form on the sheltered leeward side of the mountain.",
       "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Bietschhorn_from_L%C3%B6tschen_Pass.jpg/1024px-Bietschhorn_from_L%C3%B6tschen_Pass.jpg"
-    }
-  }
+  },
     "virga": {
       "description": "Virga, from the Latin for 'rod' or 'branch' appear as light wisps which are attached to the base of a cloud and are often at their most striking when lit by a red sunset with a light wind extending the tail into a angled curve.",
       "formation": "Simply put, virga are trails of precipitation that fall from the underside of a cloud but evaporate or sublime before it can reach the earth's surface. This happens when falling rain or ice passes through an area of dry or warm air.",
       "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Virga_clouds_in_Sweden.jpg/1024px-Virga_clouds_in_Sweden.jpg"
-    }
+    },
     "holepunch": {
       "description": "A fallstreak hole (also known as a holepunch cloud) forms when part of the cloud layer forms ice crystals which are large enough to fall as a fallstreak. They form in clouds of supercooled water droplets, water below 0 °C but not yet frozen. These water droplets need a tiny particle, a nucleus, to freeze or to be cooled below -40 °C.",
       "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/d/dc/HolePunchCloud.jpg"
-    }
+    },
     "asperitas": {
       "description": "Asperitas (formerly referred to as Undulatus Asperitas) is a distinctive, but relatively rare cloud formation that takes the appearance of rippling waves. These wave-like structures form on the underside of the cloud to makes it look like a rough sea surface when viewed from below.",
       "formation": "The way in which asperitas clouds form is somewhat a mystery, yet there is much debate and confusion over how the wave-like clouds come into existence. It is hypothesised that their appearance is associated with the aftermath of convective thunderstorms, though they have also been sighted in relatively calm environments. One theory does suggest that they are formed when mammatus clouds descend into areas of the sky where wind direction changes with height causing the wave-like movement.",
       "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/c/c5/Undulatus_asperatus.jpg"
-    }
+    },
     "nacreous": {
       "description": "Nacreous clouds form in the lower stratosphere over polar regions when the sun is just below the horizon. The clouds are illuminated from below and often glow in vivid colours and will often remain visible for a couple of hours after sunset and through the night as they are lit by moonlight",
       "formation": "The water droplets which form nacreous clouds are much smaller than those forming more common clouds. The smaller droplets scatter light in a different way which is what creates the distinctive luminescent appearance.",
       "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/c/cc/Nacreous_clouds_Antarctica.jpg"
-    }
+    },
     "kelvin-heimholtz": {
       "description": "Kelvin-Helmholtz is an extremely rare phenomenon where a cloud produces a billowing wave pattern. The distinctive cloud occurs when there is a strong vertical shear between two air streams causing winds to blow faster at the upper level than at the lower levels.",
       "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Kelvin-Helmholtz_waves.jpg/1024px-Kelvin-Helmholtz_waves.jpg"
     }
   }
-}
 
 export default data
